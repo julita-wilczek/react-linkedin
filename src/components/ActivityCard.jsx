f62742d8b69fda11c87296f07c50646a76ce72b5
J. Wilczek <85438277+julita-wilczek@users.noreply.github.com>
2022-02-21 17:03:50 +0100

    Update ActivityCard.jsx

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -7,11 +7,11 @@ const ActivityCard = () => {
         <div>
           <a href="/">0 followers</a>{" "}
         </div>
-        <button type="button" class="btn btn-outline-primary">Start a post</button>
+        <button type="button" className="btn btn-outline-primary">Start a post</button>
       </div>
       <div className="font-weight-normal mb-1">You haven't posted lately</div>
       <div className="font-weight-lighter mb-4">Recent posts you share or comment on will be displayed here</div>
-      <button type="button" class="btn btn-outline-secondary  btn-lg btn-block">see all activity</button>
+      <button type="button" className="btn no-border btn-lg btn-block">see all activity</button>
 </>
 
     
