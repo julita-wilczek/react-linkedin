01bdb10a2fc52a5833c14e5353978ee1fedd6ae3
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2022-02-21 16:43:51 +0100

    commit

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -1,5 +1,21 @@
-const ActivityCard = () => {
-    return(<div>Here will go Activity Card</div>)
-}
+import { Button } from "react-bootstrap";
 
-export default ActivityCard
+
+const ActivityCard = () => {
+  return (
+<>      <div className="d-flex align-items-center justify-content-between">
+        <div>
+          <a href="/">0 followers</a>{" "}
+        </div>
+        <button type="button" class="btn btn-outline-primary">Start a post</button>
+      </div>
+      <div className="font-weight-normal mb-1">You haven't posted lately</div>
+      <div className="font-weight-lighter mb-4">Recent posts you share or comment on will be displayed here</div>
+      <button type="button" class="btn btn-outline-secondary  btn-lg btn-block">see all activity</button>
+</>
+
+    
+  );
+};
+
+export default ActivityCard;
