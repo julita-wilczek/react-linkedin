c65104a369d8b58c4c3b5b520aede2c75c182f32
Lucai994 <luca.muratori.94@hotmail.it>
2022-02-21 17:04:17 +0100

    commit the people-mayknow and also-viewed part

--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -1,5 +1,24 @@
-const PeopleCard = () => {
-    return(<div> Here goes some picture and info</div>)
+import MayKnow from '../data/may-know'
+import AlsoViewed from '../data/also-viewed'
+import {Button, Card} from 'react-bootstrap'
+import './PeopleCard.css'
+
+const PeopleCard = ({array}) => {
+    return(
+        // array.map(people=>{return(
+        // <Card className='d-flex flex-row'>
+        //     <Card.Img variant="top" id='userImageMayKnow' src={people.img} />
+        //     <Card.Body>
+        //         <Card.Title id='PeopleName'>{people.name}</Card.Title>
+        //         <Card.Text id='PeopleDescription'>
+        //         {people.description}
+        //         </Card.Text>
+        //         <Button variant="light" className='border rounded-pill'>Connect</Button>
+        //     </Card.Body>
+        // </Card>
+        // )})
+        <div>Hello</div>
+    )
 }
 
 export default PeopleCard
--- src/components/PeopleSection.jsx
+++ src/components/PeopleSection.jsx
@@ -1,13 +1,24 @@
 // use the array props to link to a proper array
 // array needs to be created
-
+import PeopleYouMayKnow from '../data/may-know'
+import AlsoViewed from '../data/also-viewed'
 import PeopleCard from "./PeopleCard"
 const PeopleSection = ({name, array}) => {
-    return (
-    <div><h6>{name}</h6>
-    <PeopleCard />
-    </div>)
-    
+    // if (array==='may-know'){
+    //     return (
+    //         <div><h6>{name}</h6>
+    //         <PeopleCard array={AlsoViewed} />
+    //         </div>
+    // )
+    // } else (array==='also-know'){
+    //     return (
+    //         <div><h6>{name}</h6>
+    //         <PeopleCard array={PeopleYouMayKnow} />
+    //         </div>
+    // )}
+    return(
+        <div>Hello</div>
+    )
     }
     
     export default PeopleSection
